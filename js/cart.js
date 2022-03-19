7d6d4446409cc3ccc12cf473b43f0b0a71624541
Gurpreet Sarangal <gurpreetsarangal7@gmail.com>
2022-03-19 11:04:25 +0530

    added : 'add one to the cart' functionality

--- js/cart.js
+++ js/cart.js
@@ -41,6 +41,19 @@ function sign_out(){
     window.location.replace("index.html");
 }
 
+function add_one(id){
+    logged_user = localStorage.getItem("logged_in_user");
+    user_data = JSON.parse(localStorage.getItem(logged_user));
+    for(var i=0; i<user_data.cart.length; i++){
+        if(user_data.cart[i].id == id){
+            user_data.cart[i].quantity += 1;
+            localStorage.setItem(logged_user, JSON.stringify(user_data));
+
+            document.getElementById('item-quantity').value = user_data.cart[i].quantity;
+        }
+    }
+}
+
 function remove_all(id){
     logged_user = localStorage.getItem("logged_in_user");
     user_data = JSON.parse(localStorage.getItem(logged_user));
@@ -65,11 +78,15 @@ function generate_cart(){
         document.getElementById("heading").innerHTML = '<h2>Your Cart is Empty.</h2><a href="index.html">Start Shopping!!</a>';
         return;
     }
+    initial_html = document.getElementById('rack').innerHTML;
+    initial_html += '<ul id="cart-list"></ul>';
+    document.getElementById('rack').innerHTML = initial_html;
+
     for(var i=0; i<user_data.cart.length; i++){
-        var inner_div = document.getElementById("cart-list").innerHTML;
+        var inner_li = document.getElementById("cart-list").innerHTML;
         var id = (user_data.cart[i]).id;
-        inner_div += '<div class="rack-item col-3" id="'+id+'" ></div>';
-        document.getElementById("cart-list").innerHTML = inner_div;
+        inner_li += '<li class="rack-item" id="'+id+'" ></li>';
+        document.getElementById("cart-list").innerHTML = inner_li;
 
         var quantity = (user_data.cart[i]).quantity;
         for(var j=0; j<products.length; j++){
@@ -79,7 +96,7 @@ function generate_cart(){
                 break;
             }
         }
-        inner_div =  '<div class="item-pic">  <img src="/Pictures/logo'+String(id)+'.png" alt="Item-pic"></div><div class="item-name">  <span>'+String(name)+'</span></div> <div id="Qnt-'+String(id)+'"><span>Quantity: '+quantity+'  </span></div><div class="item-price">  <span>Price: '+String(price)+' &#36; </span></div><div class="remove-it"><input type="button" value="Remove One" id="remove-one-'+String(id)+'-from-cart" onclick="remove_one('+String(id)+')"><input type="button" value="Remove All" id="remove-all-'+String(id)+'-from-cart" onclick="remove_all('+String(id)+')"></div>';
+        inner_div =  '<div class="list-item"><div><img src="./Pictures/logo'+id+'.png" alt="pic" class="item-img"></div><div class="item-name"><span>'+name+'</span></div><div class="quantity"><input type="button" class="inc-dec" value="  +  " onclick="add_one('+id+')"><input type="number" id="item-quantity" value="'+quantity+'"><input type="button" class="inc-dec" value="  -  " onclick="remove_one('+id+')"></div><div class="price"><span>&#36;'+price+'</span></div><div class="remove"><input type="button" value="Remove" onclick="remove_all('+id+')"></div></div>';
         document.getElementById(id).innerHTML = inner_div;
     }
 
